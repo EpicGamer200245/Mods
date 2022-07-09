3fc1922a145afa9c714b7709c3ffb275d27727fe
EpicGamer200245 <56707910+EpicGamer200245@users.noreply.github.com>
2022-07-09 14:33:34 -0600

    Beta v2

--- SandboxML/main.cs
+++ SandboxML/main.cs
@@ -1,16 +1,49 @@
 ﻿using MelonLoader;
 using HarmonyLib;
+using UnityEngine;
+using System.Reflection;
+using TMPro;
+using UnityEngine.UI;
+using MarchingBytes;
+using System;
+using UnityEngine.Events;
 
-[assembly: MelonInfo(typeof(SandBox.Sandbox), "Sandbox", "0.5.0", "EpicGamer")]
+
+[assembly: MelonInfo(typeof(SandBox.Sandbox), "Sandbox", "0.9.0", "EpicGamer")]
 [assembly: MelonGame("Sayan", "Apes vs Helium")]
 
 namespace SandBox
 {
+    public static class Private
+    {
+        public static void SetPrivateValue<T>(this T type, string field, object value)
+        {
+            type.GetType().GetField(field, BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public).SetValue(type, value);
+        }
+    }
+
     public class Sandbox : MelonMod
     {
+
         public override void OnApplicationStart()
         {
-            MelonLogger.Msg("Sandbox loaded...");
+            LoggerInstance.Msg("Sandbox loaded...");
+        }
+        [HarmonyPatch(typeof(WaveSpawner), "Update")]
+        public class WaveSpawnerUpdate_Patch
+        {
+            [HarmonyPrefix]
+            public static void OnUpdate(ref WaveSpawner __instance)
+            {
+                if (Input.GetKeyDown(KeyCode.G))
+                {
+                    __instance.SetPrivateValue("state", WaveSpawner.SpawnState.COUNTING);
+                    __instance.SetPrivateValue("waveCountDown", __instance.timeBetweenWaves);
+                    __instance.WaveFinishedEvent.Invoke();
+                    __instance.nextwave++;
+                    __instance.waveNumberText.text = (__instance.nextwave + 1).ToString() + "/" + __instance.lastRound;
+                }
+            }
         }
 
         [HarmonyPatch(typeof(Currency), "UpdateCurrency")]
@@ -33,7 +66,7 @@ namespace SandBox
         public class CurrencyStart_Patch
         {
             [HarmonyPrefix]
-            public static bool Prefix(ref Currency __instance)
+            public static bool Prefix(Currency __instance)
             {
                 __instance.currency = int.MaxValue;
                 __instance.UpdateCurrency(0, false);
